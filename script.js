113eaa672c1be5d203163c06426a3cdd791ac6c7
NimmaSahasra2631 <nimmasahasra2005@gmail.com>
2025-08-04 18:59:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("visit-btn").addEventListener("click", function () {
+  alert("You are about to visit Blinkit.");
+  window.open("https://blinkit.com", "_blank"); // Opens in new tab
+});
